6bc54da943f3c7f0a4b586a0acb2d28395b76afd
Josh Brown <joshbrown31@gmail.com>
2022-05-24 10:44:10 -0400

    commit

---
+++ may_2022/w4_d2.js
@@ -0,0 +1,28 @@
+//create a function that accepts a string representing an int
+//in binary notation, and returns the int
+//you do not need to use parseInt
+//should return integer >= 0
+
+//given "1010101", return 85
+//given "100011", return 35
+
+const binToDec = (str) => {
+}
+
+console.log(binToDec("1010101"));
+console.log(binToDec("100011"));
+console.log(binToDec("100"));
+console.log(binToDec("1000"));
+console.log(binToDec("1111"));
+console.log(binToDec("110101011"));
+console.log(binToDec("1111111111"));
+
+//if you have the time, do the opposite! Take in a decimal number and return a
+//string with the binary data
+const decToBin = (num) => {
+}
+
+// console.log(decToBin(85));
+// console.log(decToBin(420));
+// console.log(decToBin(11));
+// console.log(decToBin(181));
