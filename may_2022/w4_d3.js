2c35af6c15c3a0e26a752c0eca09e9f57ee2e85d
Josh Brown <joshbrown31@gmail.com>
2022-05-25 11:21:09 -0400

    commit

---
+++ may_2022/w4_d3.js
@@ -0,0 +1,34 @@
+/* 
+Take in a string representing an int in hexidecimal notation, 
+and return the value in decimal notation. 
+Given "1D2", return 466.
+*/
+
+//0123456789ABCDEF
+
+//7D + 1 = 7E
+//9F + 1 = A0
+
+//digits = powers of 16 rather than powers of 10
+
+const hexStrToDec = (str) => {
+    //convert characters to equivalent hex numbers
+    //multiply by powers of 16 and add them together
+}
+
+//console.log(hexStrToDex("1D2"));
+//console.log(hexStrToDex("2C1"));  //expected output: 705
+//console.log(hexStrToDex("3B5"));
+//console.log(hexStrToDex("FFF"));
+
+/* 
+Given an integer, return a string representing the hexadecimal value.
+Given 108, return 6C.
+*/
+
+const dectoDexStr = (val) => {
+    //code
+}
+
+//console.log(dexToDexStr(108));
+//console.log(dexToDexStr(420));
