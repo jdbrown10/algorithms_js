7fbd2eff9a7e7d27c4fbc44b818fa6a473f1f96f
Josh Brown <joshbrown31@gmail.com>
2022-05-27 15:13:24 -0400

    commit

---
+++ may_2022/w4_d5.js
@@ -0,0 +1,42 @@
+//Riddles!
+
+/* 
+There is a barrel with no lid and some wine in it. "This barrel of wine is more than half full," said Curly. "No it's not," says Mo. "It's less than half full." Without any measuring implements and without removing any wine from the barrel, how can they easily determine who is correct?
+ */
+
+/* I was visiting a friend one evening and remembered that he had three daughters. I asked him how old they were. “The product of their ages is 72,” he answered. Quizzically, I asked, “Is there anything else you can tell me?” “Yes,” he replied, “the sum of their ages is equal to the number of my house.” I stepped outside to see what the house number was. Upon returning inside, I said to my host, “I’m sorry, but I still can’t figure out their ages.” He responded apologetically, “I’m sorry, I forgot to mention that my oldest daughter likes strawberry shortcake.” With this information, I was able to determine all three of their ages. How old is each daughter? 
+*/
+
+
+
+
+/* 
+3, 3, and 8. The only groups of 3 factors of 72 to have non-unique sums are “2 6 6” and “3 3 8” (with a sum of 14). The rest have unique sums:
+
+2 + 2 + 18 = 22
+2 + 3 + 12 = 18
+2 + 4 + 9 = 15
+3 + 4 + 6 = 13
+
+The house number alone would have identified any of these groups. Since more information was required, we know the sum left the answer unknown. The presence of a single oldest child eliminates “2 6 6”, leaving “3 3 8” as the only possible answer.
+*/
+
+//====================================================
+
+// B || W B W <-colors
+// A || B C D <-labels
+
+//Shown above are four men buried up to their necks in the ground. They cannot move, so they can only look forward. Between A and B is a brick wall which cannot be seen through.
+
+//They all know that between them they are wearing four hats--two black and two white--but they do not know what color they are wearing. Each of them know where the other three men are buried.
+
+//In order to avoid being shot, one of them must call out to the executioner the color of their hat. If they get it wrong, everyone will get shot. They are not allowed to talk to each other and have 10 minutes to fathom it out.
+
+//After one minute, one of them calls out. Which one calls out? Why is he 100% certain of the color of his hat?
+
+//A and B can only see the wall.
+//C can only see B.
+//D can see B and C.
+
+//It's C! C can only see a white hat, but also knows that D isn't calling out, which means D must be seeing a black and white hat. Which means C must know they have a black hat.
+
