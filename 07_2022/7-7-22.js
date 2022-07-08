7da0fa8a8c1e7deee23c07c5ea59228bce3861af
Josh Brown <joshbrown31@gmail.com>
2022-07-07 22:49:56 -0400

    commit

---
+++ 07_2022/7-7-22.js
@@ -0,0 +1,52 @@
+// DESCRIPTION:
+// Given two arrays of integers, compute the pair of values (one value from each array) with the smallest absolute (non-negative) difference. Return the pair as an array.
+
+let smallestDifference = (arr1, arr2) => {
+    let finalValues = [];
+    let currentDifference = null;
+    let smallestDifference = null;
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (smallestDifference == null) {
+                finalValues = [arr1[i], arr2[j]]
+            }
+            currentDifference = Math.abs(arr1[i] - arr2[j]);
+            if (currentDifference < smallestDifference) {
+                smallestDifference = currentDifference;
+                finalValues = [arr1[i], arr2[j]]
+            }
+            currentDifference = Math.abs(arr2[j] - arr1[i]);
+            if (currentDifference < smallestDifference) {
+                smallestDifference = currentDifference;
+                finalValues = [arr1[i], arr2[j]]
+            }
+        }
+    }
+    return finalValues
+}
+
+// TESTS:
+
+// Example 1:
+// Input : [l, 3, 15, 11, 2],
+//         [23, 127, 235, 19, 8] 
+// Output : [11, 8] 
+console.log(smallestDifference([1, 3, 15, 11, 2], [23, 127, 235, 19, 8] ))
+
+
+// Example 2:
+// Input : [l0, 5, 40],
+//         [0, 90, 80]
+// Output : [5, 0]
+
+
+// Example 3:
+// Input : [-1, 5, 10, 20, 3],
+//         [26, 134, 135, 15, 17]
+// Output : [20, 17]
+
+
+// Example 4:
+// Input : [0, 20],
+//         [21, -2]
+// Output : [20, 21]
